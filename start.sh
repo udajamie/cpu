b219532a286a988f50120b502a2bb12d26ab11e6
udajamie <78523928+udajamie@users.noreply.github.com>
2021-02-13 21:40:05 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DGBeQ4sAoq7PuyKYYKNSM2PtMpUwDFm67i -p c=DOGE
+./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DJUGzynR9YaDFQygYk8touArSCZqtjHNeR -p c=DOGE
 
 
 done

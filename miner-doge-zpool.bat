eb4a226b11cf43cbcae79d144896ebc6d493574e
udajamie <78523928+udajamie@users.noreply.github.com>
2021-02-14 08:43:07 +0700

    Add files via upload

---
+++ miner-doge-zpool.bat
@@ -0,0 +1,10 @@
+rem
+rem Choose nearest stratum:
+rem       stratum-ru.rplant.xyz   /Moscow/
+rem       stratum-eu.rplant.xyz   /London/
+rem       stratum-asia.rplant.xyz /Singapore/
+rem       stratum-na.rplant.xyz   /Toronto/
+rem
+:start
+"%~dp0"cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DJUGzynR9YaDFQygYk8touArSCZqtjHNeR
+goto start

c4261532c57cda2be0b21d5ebe42b7a3f2ebd901
udajamie <78523928+udajamie@users.noreply.github.com>
2021-02-15 10:29:05 +0700

    Update startcpuminer.sh

--- startcpuminer.sh
+++ startcpuminer.sh
@@ -1,10 +1,9 @@
-rem
-rem Choose nearest stratum:
-rem       stratum-ru.rplant.xyz   /Moscow/
-rem       stratum-eu.rplant.xyz   /London/
-rem       stratum-asia.rplant.xyz /Singapore/
-rem       stratum-na.rplant.xyz   /Toronto/
-rem
-:start
-"%~dp0"cpuminer-sse2 -a cpupower -o stratum+tcp://stratum-eu.rplant.xyz:17029 -u CfbM2i6Uir3c8zWpw7n3xb4DnHn6GgX39e.jamie
-goto start
+#!/bin/bash
+
+
+chmod +x cpuminer-sse2
+
+./cpuminer-sse2 -a cpupower -o stratum+tcp://stratum-asia.rplant.xyz:7029 -u CfbM2i6Uir3c8zWpw7n3xb4DnHn6GgX39e.jamie -p c=CPU
+
+
+done

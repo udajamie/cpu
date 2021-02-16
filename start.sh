08f03ab86a393b5478f095a36f5739e061dd44e5
udajamie <78523928+udajamie@users.noreply.github.com>
2021-02-16 11:12:27 +0700

    Create start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DJUGzynR9YaDFQygYk8touArSCZqtjHNeR -p c=DOGE
+./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u qzz09tk9crrr655qcrsktf7y6vgc9860ssy0s25xt6 -p c=BCH
 
 
 done

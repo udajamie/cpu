b92b0e9b79b1fadafc53c94f0276704d5aeadd7b
udajamie <78523928+udajamie@users.noreply.github.com>
2021-02-28 20:28:40 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.na.mine.zergpool.com:4250 -u CfbM2i6Uir3c8zWpw7n3xb4DnHn6GgX39e -p c=CPU,m=solo -t 4
+./cpuminer-sse2.exe -a cpupower -o stratum+tcp://stratum-eu.rplant.xyz:7029 -u CfbM2i6Uir3c8zWpw7n3xb4DnHn6GgX39e.jamie -t 4
 
 
 done
